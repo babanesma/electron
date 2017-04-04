f1c2a068d18c77abcb95a1933b0c839d17122f6d
Yassin Mohii <yassin.mohii@gmail.com>
2017-04-04 12:16:12 +0200

    - removing commented out source code.

--- atom/browser/native_window_views.cc
+++ atom/browser/native_window_views.cc
@@ -7,7 +7,6 @@
 #include <string>
 #include <vector>
 #include <stdlib.h>
-//#include "stdafx.h"
 
 #include "atom/browser/ui/views/menu_bar.h"
 #include "atom/browser/ui/views/menu_layout.h"
@@ -82,62 +81,6 @@ void FlipWindowStyle(HWND handle, bool on, DWORD flag) {
   ::SetWindowLong(handle, GWL_STYLE, style);
 }
 #endif
-
-// #if defined(OS_WIN)
-// HHOOK hKeyboardHook;
-
-// LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) 
-// {
-//     PKBDLLHOOKSTRUCT p;
-
-//     if (nCode == HC_ACTION) 
-//     {
-//         p = (PKBDLLHOOKSTRUCT) lParam;
-
-//         if (
-//             // WIN key (for Start Menu)
-//             ((p->vkCode == VK_LWIN) || (p->vkCode == VK_RWIN)) ||       
-//             // ALT+TAB
-//             (p->vkCode == VK_TAB && p->flags & LLKHF_ALTDOWN) ||       
-//             // ALT+ESC
-//             (p->vkCode == VK_ESCAPE && p->flags & LLKHF_ALTDOWN) ||    
-//             // CTRL+ESC
-//             ((p->vkCode == VK_ESCAPE) && ((GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0)) ||
-//             // CTRL+SHIFT+ESC
-//             ((p->vkCode == VK_ESCAPE) && ((GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0) && ((GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0)) ||
-//       // CTRL+ALT+DEL (Unfortunately doesn't work !)
-//             ((p->vkCode == VK_DELETE) && ( (p->flags & LLKHF_ALTDOWN) != 0 ) && ( (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0))
-//             )
-//             return 1;
-//    }
-                    
-//    return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
-// }
-
-// int TaskSwitching_Enable_Disable(BOOL bEnableDisable)
-// {
-//     if (!bEnableDisable)
-//     {
-//         if (!hKeyboardHook)
-//     {
-//             // Install the low-level keyboard hook
-//             hKeyboardHook  = SetWindowsHookEx(WH_KEYBOARD_LL, 
-//                                               LowLevelKeyboardProc, 
-//                                               hInst, 
-//                                               0);
-//       if (!hKeyboardHook)
-//         return 0;
-//     }
-//     }
-//     else
-//     {
-//         UnhookWindowsHookEx(hKeyboardHook);
-//         hKeyboardHook = NULL;
-//     }
-
-//     return 1;
-// }
-// #endif
 
 bool IsAltKey(const content::NativeWebKeyboardEvent& event) {
   return event.windowsKeyCode == ui::VKEY_MENU;
@@ -819,25 +762,6 @@ KBDLLHOOKSTRUCT kbdStruct;
 // a key is pressed.
 LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    // if (nCode >= 0)
-    // {
-    //     // the action is valid: HC_ACTION.
-    //     if (wParam == WM_KEYDOWN)
-    //     {
-    //         // lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
-    //         kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
-    //         // a key (non-system) is pressed.
-    //         if (kbdStruct.vkCode == VK_F1)
-    //         {
-    //             // F1 is pressed!
-    //             MessageBoxA(NULL, "F1 is pressed!", "key pressed", MB_ICONINFORMATION);
-    //         }
-    //     }
-    // }
-
-    // // call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
-    // return CallNextHookEx(_hook, nCode, wParam, lParam);
-    // 
     PKBDLLHOOKSTRUCT p;
 
     if (nCode == HC_ACTION) 
@@ -866,11 +790,6 @@ LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 
 void SetHook()
 {
-    // Set the hook and set it to use the callback function above
-    // WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
-    // The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
-    // function that sets and releases the hook. If you create a hack you will not need the callback function 
-    // in another place then your own code file anyway. Read more about it at MSDN.
     _hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0);
     if (!_hook)
     {
